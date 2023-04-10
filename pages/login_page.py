aa8c73506a9b99ad545d82cc001679d07c6582c3
eturov <eugeneturov977@gmail.com>
2023-04-10 16:53:19 +0300

    update login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,6 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert 'login' in self.browser.current_url, f'URL is not correct. Should be login URL.'
 
     def should_be_login_form(self):
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,4 @@
 from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -8,24 +7,3 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
-
-
-def test_opened_logreg_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-
-
-def test_have_login_form(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-
-
-def test_have_reg_form(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
