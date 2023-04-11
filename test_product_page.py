b0e3fa8e6d72a5d1224422daef6f0ac2df59f741
eturov <eugeneturov977@gmail.com>
2023-04-12 00:31:43 +0300

    mark trivial bug

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,9 @@ from .pages.product_page import ProductPage
 import pytest
 
 
-@pytest.mark.parametrize('num_promo', [str(i) for i in range(0, 10)])
+@pytest.mark.parametrize('num_promo', ['0', '1', '2', '3', '4', '5', '6',
+                                       pytest.param('7', marks=pytest.mark.xfail(reason='known trivial bug')),
+                                       '8', '9'])
 def test_guest_can_add_product_to_basket(browser, num_promo):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}'
     page = ProductPage(browser, link)
