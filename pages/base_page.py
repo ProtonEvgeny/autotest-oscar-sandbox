5163681a80a105c47c12247563bd8509758a2eea
eturov <eugeneturov977@gmail.com>
2023-04-14 20:47:10 +0300

    add login page methods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from .locators import BasePageLocators
 import math
 
 
@@ -48,3 +49,10 @@ class BasePage:
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), 'Login link is not presented'
--- test_product_page.py
+++ test_product_page.py
@@ -16,7 +16,7 @@ def test_guest_can_add_product_to_basket(browser, num_promo):
     page.should_be_basket_price()
 
 
-@pytest.mark.xfail(reason='known bug')
+@pytest.mark.skip(reason='known bug')
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
@@ -32,10 +32,17 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
-@pytest.mark.xfail(reason='known bug')
+@pytest.mark.skip(reason='known bug')
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.item_add_to_basket()
     page.should_disappeared_success_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
