b2a23c6f3beb57d1373f05a7ec23c19ce81b8516
eturov <eugeneturov977@gmail.com>
2023-04-14 03:22:21 +0300

    add negative checks

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,6 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 import math
 
 
@@ -32,3 +33,18 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(ec.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, ignored_exceptions=[TimeoutException]).\
+                until_not(ec.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
