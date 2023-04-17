29c8544da623d6af088d6a5370a32012921947a9
eturov <eugeneturov977@gmail.com>
2023-04-17 20:21:06 +0300

    updated basket page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,2 +1,11 @@
 from .base_page import BasePage
-from .locators import BasePageLocators
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_not_be_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), \
+            'Items in basket is presented, but should not be'
+
+    def should_be_empty_basket_message(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_TEXT), 'Empty basket message is not presented'
