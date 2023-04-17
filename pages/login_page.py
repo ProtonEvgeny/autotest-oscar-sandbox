39164bd520225e44eefd8607ebac85e46b85f6fb
eturov <eugeneturov977@gmail.com>
2023-04-18 02:46:38 +0300

    add register user

--- pages/login_page.py
+++ pages/login_page.py
@@ -16,3 +16,14 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), f'Registration form is not presented'
+
+    def register_new_user(self, email: str, password: str):
+        self.should_be_login_page()
+        email_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL)
+        email_field.send_keys(email)
+        pass1_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASS1)
+        pass1_field.send_keys(password)
+        pass2_field = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASS2)
+        pass2_field.send_keys(password)
+        reg_button = self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON)
+        reg_button.click()
