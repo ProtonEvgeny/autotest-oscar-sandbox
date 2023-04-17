bfc2c6a45370dbe101ab515a797678568fdf836a
eturov <eugeneturov977@gmail.com>
2023-04-18 02:08:49 +0300

    update tests

--- test_main_page.py
+++ test_main_page.py
@@ -4,13 +4,21 @@ from .pages.basket_page import BasketPage
 import pytest
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/'
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/'
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
@@ -21,10 +29,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_items_in_basket()
     basket_page.should_be_empty_basket_message()
-
-
-def test_guest_should_see_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
